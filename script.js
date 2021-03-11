f1195e95bac7956313a03653b464bb3d42cc872c
Julia Chmaj <jululuu22@gmail.com>
2021-03-11 22:50:44 +0100

    final

--- script.js
+++ script.js
@@ -6,10 +6,71 @@ const botDoorPath= "https://content.codecademy.com/projects/chore-door/images/ro
 const beachDoorPath= "https://content.codecademy.com/projects/chore-door/images/beach.svg"
 const spaceDoorPath= "https://content.codecademy.com/projects/chore-door/images/space.svg"
 
-
 let openDoor1
-let openDoor2 
-let openDoor3 
+let openDoor2
+let openDoor3
+let currentlyPlaying= true;
+
+let closedDoor = "https://content.codecademy.com/projects/chore-door/images/closed_door.svg";
+
+isClicked = (door) => {
+  if (door.src === closedDoor) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+
+let numClosedDoors = 3
+playDoor = (door) => {
+  numClosedDoors --;
+  console.log(door.src);
+  if (numClosedDoors === 0 & door.src === botDoorPath ) {
+    return gameOver("win");
+  }
+  else if (numClosedDoors > 0 & door.src === botDoorPath)
+    
+    return gameOver();
+
+}
+
+let startButton = document.querySelector(".start-button");
+let currentStreak = 0;
+gameOver = (status) => {
+  
+  if (status === "win"){
+    currentStreak++;
+    document.querySelector(".current-streak-score").innerText = currentStreak;
+  
+    startButton.innerText= 'You win! Play again?';
+
+  }
+  else {
+    startButton.innerText= 'Game Over! Try Again :)';
+    currentStreak= 0;
+    document.querySelector(".current-streak-score").innerText = currentStreak;
+    
+
+    
+  }
+
+  let bestStreak = parseInt(document.querySelector(".best-streak-score").innerText);
+  if (currentStreak > bestStreak) {
+    bestStreak = currentStreak;
+    document.querySelector(".best-streak-score").innerText = bestStreak;
+  }
+
+  
+  currentlyPlaying= false;
+
+
+}
+
+
+
+
 randomChoreDoorGenerator = () => {
   let choreDoor= Math.floor(Math.random()*3)
 
@@ -27,22 +88,56 @@ randomChoreDoorGenerator = () => {
     openDoor3 = botDoorPath;
     openDoor2 = beachDoorPath;
     openDoor1 = spaceDoorPath;
+
   }
-  
 
 }
 
+
+doorImage1.onclick = () => {
+  if (!isClicked(doorImage1) & currentlyPlaying){
+    doorImage1.src=openDoor1;
+    playDoor(doorImage1);
+  }
+
+  };
+doorImage2.onclick = () => {
+  if (!isClicked(doorImage2) & currentlyPlaying){
+    doorImage2.src=openDoor2;
+    playDoor(doorImage2)
+    };
+}
+doorImage3.onclick = () => {
+  if (!isClicked(doorImage3) & currentlyPlaying){
+    doorImage3.src=openDoor3;
+    playDoor(doorImage3)
+  };
+}
+
+
+
+startRound = () => {
+  doorImage1.src = closedDoor;
+  doorImage2.src = closedDoor;
+  doorImage3.src = closedDoor;
+  numClosedDoors=3;
+  startButton.innerHTML = "Good luck!"
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+
+}
+
+
+startButton.onclick = () => {
+    if (!currentlyPlaying) {
+         startRound();
+    }
+    else {
+        alert("Choose the door to open")
+    }
+}    
+
 randomChoreDoorGenerator()
-
-doorImage1.onclick = () => {doorImage1.src=openDoor1}
-doorImage2.onclick = () => {doorImage2.src=openDoor2}
-doorImage3.onclick = () => {doorImage3.src=openDoor3}
-
-
-
-
-
-
 
 
 
